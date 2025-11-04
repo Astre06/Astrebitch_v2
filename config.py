a45cb3f92e5c7bf9e7a2f0f2c913b0bc0d0d8fec
Astre06 <122554381+Astre06@users.noreply.github.com>
2025-11-04 20:05:41 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import json
 
 # Default values
 DEFAULTS = {
-    "BOT_TOKEN": "7942038573:AAEiCbiuw41GN5HRkQRyQ4Ydz4bdwfGbzpM",
+    "BOT_TOKEN": "8340045274:AAG4PaDRFdhY62niUkoa5caTTLR-e76pxa0",
     "CHANNEL_ID": "-1002963282470",
     "ADMIN_ID": "6679042143",
     "DEFAULT_API_URL": "https://www.seed2design.co.uk",
@@ -58,4 +58,5 @@ RETRY_COUNT = _cfg["RETRY_COUNT"]
 MAX_WORKERS = _cfg["MAX_WORKERS"]
 BATCH_SIZE = _cfg["BATCH_SIZE"]
 DELAY_BETWEEN_BATCHES = _cfg["DELAY_BETWEEN_BATCHES"]
+
 
